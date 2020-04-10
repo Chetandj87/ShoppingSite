68ae2d0a7cc9e53303cf6bddaae22acee5b42b3d
Chetan <Chetan@DESKTOP-MNMNLH8>
2020-04-10 22:11:01 +0530

    Added Email Modules

--- ShoppingSiteWeb/src/main/java/com/shop/controller/UserController.java
+++ ShoppingSiteWeb/src/main/java/com/shop/controller/UserController.java
@@ -1,14 +1,36 @@
 package com.shop.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.shop.model.User;
+import com.shop.service.UserService;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
 	
+	@Autowired
+	UserService userService;
+	
 	@RequestMapping("/login")
 	public String login() {
 		return "login";
+	}
+	
+	@RequestMapping("add")
+	public String register(ModelMap map) {
+		map.addAttribute("user", new User());
+		return "register";
+	}
+	
+	@RequestMapping(value = {"/add"},method = RequestMethod.POST)
+	public String addUser(@ModelAttribute("user") User user) {
+		userService.addUser(user);
+		return "redirect:/user/login";
 	}
 }
